c6018c6452f74ae42049e69226df3097d8ac1d9b
coding-beagle <nicholas.teague@focusrite.com>
2025-02-26 14:42:42 +0000

    snowfall sonatas

--- app/src/components/title.js
+++ app/src/components/title.js
@@ -1,9 +1,98 @@
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
 import defaultColours from "../themes/themes";
-import Tracks from "./circuit_track";
 
 export default function Title() {
   const [isHover, setIsHover] = useState(false);
+  let particles = [];
+  const particleCount = 500;
+  const gravity = 0.05;
+  const maxSpeed = 1;
+
+  useEffect(() => {
+    const canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const ctx = canvas.getContext("2d");
+
+    class Particle {
+      constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.vx = Math.random() * 2 - 1;
+        this.vy = Math.random() * 2 - 1;
+        this.size = Math.random() * 2 + 1;
+        this.color = defaultColours.accent;
+        this.gravity = gravity;
+      }
+
+      changeDirection() {
+        this.gravity *= -1;
+      }
+
+      update() {
+        this.vy += this.gravity;
+        this.x += this.vx;
+        this.y += this.vy;
+
+        if (this.y + this.size > canvas.height) {
+          this.y = 0;
+          this.vy *= -0.5;
+        }
+
+        if (this.y + this.size < 0) {
+          this.y = canvas.height;
+          this.vy *= -0.5;
+        }
+
+        if (this.vy > maxSpeed) {
+          this.vy -= Math.random(0.1, 0.5);
+        }
+
+        if (this.vy < maxSpeed) {
+          this.vy += Math.random(0.1, 0.5);
+        }
+
+        if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+          this.vx *= -1;
+        }
+      }
+
+      draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+
+    function initParticles() {
+      for (let i = 0; i < particleCount; i++) {
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        particles.push(new Particle(x, y));
+      }
+    }
+
+    function animate() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      particles.forEach((particle) => {
+        particle.update();
+        particle.draw();
+      });
+      requestAnimationFrame(animate);
+    }
+
+    initParticles();
+    animate();
+  }, []);
+
+  useEffect(() => {
+    console.log("hovering", isHover);
+    for (let i = 0; i < particles.length; i++) {
+      particles[i].changeDirection();
+    }
+  }, [isHover, particles]);
 
   return (
     <div
@@ -16,24 +105,19 @@ export default function Title() {
         position: "relative",
       }}
     >
-      <svg
-        width="100%"
-        height="100%"
+      <canvas
         style={{
           position: "absolute",
           top: 0,
           left: 0,
           zIndex: 1,
         }}
-      >
-        <Tracks hovering={isHover} line_path="M 200,50 350,50, 350,100" />
-        <Tracks hovering={isHover} line_path="M 1200,50 1350,50, 1350,100" />
-      </svg>
+      />
       <header
         onMouseEnter={() => setIsHover(true)}
         onMouseLeave={() => setIsHover(false)}
         style={{
-          fontSize: "12em",
+          fontSize: "8em",
           textAlign: "center",
           color: isHover ? defaultColours.secondary : defaultColours.accent,
           fontWeight: "bold",
