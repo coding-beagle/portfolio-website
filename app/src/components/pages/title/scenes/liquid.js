5cf6118c7da46de9fe9409c49637a921f2daace5
coding-beagle <nicholas.teague@focusrite.com>
2025-09-12 18:59:34 +0100

    give title aura

--- app/src/components/pages/title/scenes/liquid.js
+++ app/src/components/pages/title/scenes/liquid.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 import { useTheme } from "../../../../themes/ThemeProvider";
 import MouseTooltip from "../utilities/popovers";
 import { ChangerGroup } from "../utilities/valueChangers";
-import { clamp, colourToRGB, DIRECTIONS, getIndexFromBrushSize, getNeighbourIndexFromGrid, scaleValue } from "../utilities/usefulFunctions";
+import { clamp, colourToRGB, DIRECTIONS, getIndexFromBrushSize, getNeighbourIndexFromGrid, inRect, scaleValue } from "../utilities/usefulFunctions";
 import { MobileContext } from "../../../../contexts/MobileContext";
 
 export default function Liquid({ visibleUI }) {
@@ -372,7 +372,7 @@ export default function Liquid({ visibleUI }) {
             phase = Math.floor((x / gridWidth) * 100.0);
             if (y === 0) { type = cellTypes.TAP }
             else if (y === gridHeight - 1) { type = cellTypes.DRAIN }
-            else if (Math.random() > 0.95) { type = cellTypes.WALL }
+            else if (Math.random() > 0.95 && (x < gridWidth / 2.5 || x > 1.5 * gridWidth / 2.5 || y < gridHeight / 3 || y > 1.5 * gridHeight / 3)) { type = cellTypes.WALL }
             else { type = cellTypes.WATER }
 
             this.grid.push(new Water(x, y, this, type, phase));
