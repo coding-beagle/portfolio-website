f3d0fc6b4f4b29eacf5d9fad43cbc1e096c12c9d
coding-beagle <nicholas.teague@focusrite.com>
2025-09-14 13:40:16 +0100

    spec gyro v5

--- app/src/components/pages/title/scenes/ballpit.js
+++ app/src/components/pages/title/scenes/ballpit.js
@@ -312,7 +312,7 @@ export default function BallPit({ visibleUI }) {
     }
 
     const handleOrientation = (event) => {
-      gravityDirectionRef.current = event.gamma;
+      gravityDirectionRef.current = event.gamma - 90;
     }
 
     // initParticles();
